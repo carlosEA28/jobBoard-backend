5346357e94709454abf204226684521acd42ca93
carlosEA28 <carloseaduarte28@gmail.com>
2025-01-27 16:16:57 -0300

    first commit

---
+++ src/main/java/br/com/carlos/JobBoard_backend/JobBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlos.JobBoard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardBackendApplication.class, args);
+	}
+
+}
